01aac7ff8137075272fe7f88f4648478b9f15faf
samrashaikh <shaikhsamra855@gmail.con>
2024-03-06 11:43:10 -0800

    first commit

---
+++ assigment 03/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous quote and its other
+let quote = "The ultimate aim of the ego is not to see something, but to be something. ...";
+let other = "Allama Iqbal once said,";
+//printing  famouse qote and its other
+console.log(`"${quote}" - ${other}`);
---
+++ assigment 03/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its other
+
+let quote:string = "The ultimate aim of the ego is not to see something, but to be something. ...";
+
+let other:string ="Allama Iqbal once said,";
+
+//printing  famouse qote and its other
+
+console.log(`"${quote}" - ${other}`)
