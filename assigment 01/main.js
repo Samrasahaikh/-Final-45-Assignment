50883982d3b15c99f5d65d91521bd731c13de9b2
samrashaikh <shaikhsamra855@gmail.con>
2024-03-06 11:25:41 -0800

    first commit

---
+++ assigment 01/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assigment # 1
+// storing the person name in a variable
+let personName = "Samra";
+//printing a message to person
+console.log(`Hello ${personName} would you like to learn some python today?`);
